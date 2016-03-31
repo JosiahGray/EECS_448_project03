3656ff59b0913ca5828ac0d6aa7f01abd563c6ca
Liia Butler <l173b625@ku.edu>
2016-03-31 18:56:47 -0500

    Initial commit for next two java files

---
+++ GameCollection/src/PongBall.java
@@ -0,0 +1,23 @@
+import java.awt.Graphics;
+
+public class PongBall {
+	int x;
+	int y;
+	int xSpeed;
+	int ySpeed;
+	//constructor
+	public PongBall(){
+		x = 300;
+		y = 200;
+		
+		
+	}
+	//draw
+	//add spots function?
+	//reset ball function
+	//move function
+	public void move(){
+		
+	}
+
+}
---
+++ GameCollection/src/PongHuman.java
@@ -0,0 +1,29 @@
+//does all human logic
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class PongHuman {
+	int x;
+	int y;
+	int width;
+	int height;
+	int xSpeed;
+	int ySpeed;
+	//constructor
+	public PongHuman(){
+		x = 15;
+		y = 175;
+		width = 10;
+		height = 50;
+		xSpeed = 0;
+		ySpeed= 0;
+	}
+	//draw function
+	//move function
+	public void move(){
+		
+	}
+	
+
+}
