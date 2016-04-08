515d1a992f1c75e02d3e6ac04b617c170a901789
Paul McElroy <pcm@ku.edu>
2016-04-08 16:18:29 -0500

    Source usage of Liia's Timer schedule.

--- GameCollection/src/ShootySnake.java
+++ GameCollection/src/ShootySnake.java
@@ -1,10 +1,12 @@
-/*
- * 	@author Liia Butler
- *
- */
++/*
+ +*	@author Paul McElroy
+ +* Idea for extending TimerTask and using executeGameLoop as my run function
+ +* gotten from Liia Butler.
+ +*/
 import java.util.Timer;
 public class ShootySnake {
-	public ShootySnake(){ //see if this is needed
+	public ShootySnake()
+	{
 		Timer myTimer = new Timer();
 		myTimer.schedule(new ShootySnakeDriver(), 0, 10);
 	}
