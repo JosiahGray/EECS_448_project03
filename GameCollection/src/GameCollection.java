f9d9c784613cbfc5163954fdd0a6a5856d073f73
Josiah Gray <j305g268@ku.edu>
2016-04-08 14:46:03 -0500

    Add code to center initial menu frame on the screen. Add blank panel and label to make menu frame bigger.

--- GameCollection/src/GameCollection.java
+++ GameCollection/src/GameCollection.java
@@ -7,47 +7,56 @@ public class GameCollection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		createAndDisplayGUI();
 	}
-	
+
 	private static void createAndDisplayGUI()
 	{
 		//declare components
 		JFrame frame = new JFrame("Game Collection");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 		JMenuBar menuBar;
 		JMenu menu;
 		JMenuItem menuNim;
 		JMenuItem menuPong;
 		JMenuItem menuMerge;
-		
+
 		//create menu components
 		menuBar = new JMenuBar();
 		menu = new JMenu("Games");
-		
+
 		menuNim = new JMenuItem("Nim");
 		menuNim.addActionListener(nimListener());
-		
+
 		menuPong = new JMenuItem("Pong");
 		menuPong.addActionListener(pongListener());
-		
+
 		menuMerge = new JMenuItem("Merge Conflict");
 		menuMerge.addActionListener(mergeListener());
-		
+
 		//build menu
 		menu.add(menuNim);
 		menu.add(menuPong);
 		menu.add(menuMerge);
-		
 		menuBar.add(menu);
-		
+
+		//create empty components for spacing
+		JPanel emptyPanel;
+		JLabel emptyLabel;
+		emptyPanel = new JPanel();
+		emptyLabel = new JLabel("                                        ");
+		emptyPanel.add(emptyLabel);
+
+		//add components to frame
 		frame.setJMenuBar(menuBar);
-		
+		frame.getContentPane().add(emptyPanel);
+
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
+
 	private static ActionListener nimListener()
 	{
 		ActionListener listener = new ActionListener()
@@ -57,22 +66,23 @@ public class GameCollection {
 				/*
 				 * put code to run Nim here
 				 */
-				
+
 				//set up Nim game
 				JFrame nimFrame = new JFrame("Nim");
+				nimFrame.setLocationRelativeTo(null);
 				//nimFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit when closed
 				Nim nimGame = new Nim();
-				
+
 				nimFrame.getContentPane().add(nimGame.getContent());
 				nimFrame.pack();
 				nimFrame.setVisible(true);
-				
+
 			}
 		};
-		
+
 		return listener;
 	}
-	
+
 	private static ActionListener pongListener()
 	{
 		ActionListener listener = new ActionListener()
@@ -85,10 +95,10 @@ public class GameCollection {
 				Pong pongGame = new Pong();
 			}
 		};
-		
+
 		return listener;
 	}
-	
+
 	private static ActionListener mergeListener()
 	{
 		ActionListener listener = new ActionListener()
@@ -100,7 +110,7 @@ public class GameCollection {
 				 */
 			}
 		};
-		
+
 		return listener;
 	}
 }
