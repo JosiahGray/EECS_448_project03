97c0ed89ddea0168a123b968935544105ac4323a
Liia Butler <l173b625@ku.edu>
2016-04-08 20:32:38 -0500

    Add comments to pong game

--- GameCollection/src/PongGame.java
+++ GameCollection/src/PongGame.java
@@ -4,36 +4,34 @@ import java.util.TimerTask;
 /**
  * 
  * @author lbutler
- */
-/**
- * 
  * TimerTask extension that runs the pong game using pong control
  *
  */
 public class PongGame extends TimerTask {
 	//constructor
 	/**
-	 * 
+	 * The PongHuman that will be playing in the game
 	 */
 	PongHuman human;
 	/**
-	 * 
+	 * The PongComputer that will be implementing the AI
 	 */
 	PongComputer computer;
 	/**
-	 * 
+	 * The PongBall that will be controlled in the game
 	 */
 	PongBall ball;
 	/**
-	 * 
+	 * The graphics component that will be used in drawing the graphics
 	 */
 	Graphics g;
 	/**
-	 * 
+	 * PongControl that controls pong
 	 */
 	PongControl control;
 	/**
 	 * Custom constructor for the Pong game
+	 * Creates new human, computer, ball, and control instances for the game
 	 */
 	public PongGame(){
 		human = new PongHuman();
@@ -43,7 +41,8 @@ public class PongGame extends TimerTask {
 	}
 	//step function
 	/**
-	 * 
+	 * The run function for the PongGame, calls control to run the actual Pong logic
+	 * @post calls control's run method with this game instance
 	 */
 	public void run(){
 		control.run(this);
